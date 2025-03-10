29b945d3358378e78608719e08f7f65bf31903ba
Benjamin Bennett <bennett.d.ben@gmail.com>
2025-03-10 16:55:32 +0000

    Updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,14 +8,14 @@ import org.springframework.boot.gradle.tasks.run.BootRun
 
 plugins {
     id("com.avast.gradle.docker-compose") version "0.17.12"
-    id("com.google.protobuf") version "0.9.4"
+    id("com.google.protobuf") version "0.9.5"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.flywaydb.flyway") version "11.3.4"
-    id("org.springframework.boot") version "3.4.3"
-    kotlin("jvm") version "2.1.10"
-    kotlin("plugin.allopen") version "2.1.10"
-    kotlin("plugin.jpa") version "2.1.10"
-    kotlin("plugin.spring") version "2.1.10"
+    id("org.flywaydb.flyway") version "11.7.1"
+    id("org.springframework.boot") version "3.4.4"
+    kotlin("jvm") version "2.1.20"
+    kotlin("plugin.allopen") version "2.1.20"
+    kotlin("plugin.jpa") version "2.1.20"
+    kotlin("plugin.spring") version "2.1.20"
 }
 
 group = "net.synaptology"
@@ -35,19 +35,19 @@ repositories {
 val agent = configurations.create("agent")
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-parent:3.4.3")
-    implementation("org.springframework.boot:spring-boot-maven-plugin:3.4.3")
+    implementation("org.springframework.boot:spring-boot-starter-parent:3.4.4")
+    implementation("org.springframework.boot:spring-boot-maven-plugin:3.4.4")
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     implementation("net.devh:grpc-spring-boot-starter:3.1.0.RELEASE")
-    implementation("com.google.protobuf:protobuf-java:4.30.0")
+    implementation("com.google.protobuf:protobuf-java:4.30.2")
     implementation("io.grpc:grpc-protobuf")
     implementation("io.grpc:grpc-stub")
     implementation("io.grpc:grpc-netty")
     implementation("io.grpc:grpc-kotlin-stub:1.4.1")
     implementation("jakarta.annotation:jakarta.annotation-api:3.0.0")
-    implementation("io.github.oshai:kotlin-logging-jvm:7.0.4")
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.6")
     // Data persistence
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -57,19 +57,19 @@ dependencies {
     implementation("org.flywaydb:flyway-database-postgresql")
     // OTEL
     implementation("io.opentelemetry:opentelemetry-api")
-    agent("io.opentelemetry.javaagent:opentelemetry-javaagent:2.13.3")
+    agent("io.opentelemetry.javaagent:opentelemetry-javaagent:2.15.0")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(module = "mockito-core")
     }
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("io.grpc:grpc-testing:1.70.0")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+    testImplementation("io.grpc:grpc-testing:1.71.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testImplementation("org.junit.jupiter:junit-jupiter-engine")
     testImplementation("com.ninja-squad:springmockk:4.0.2")
 
-    testImplementation("org.testcontainers:testcontainers:1.20.5")
+    testImplementation("org.testcontainers:testcontainers:1.20.6")
     testImplementation("org.testcontainers:junit-jupiter:1.20.6")
 }
 
@@ -96,17 +96,17 @@ tasks.withType<Test> {
 
 configure<DependencyManagementExtension> {
     imports {
-        mavenBom("io.grpc:grpc-bom:1.70.0")
+        mavenBom("io.grpc:grpc-bom:1.72.0")
     }
 }
 
 protobuf {
     protoc {
-        artifact = "com.google.protobuf:protoc:4.30.0"
+        artifact = "com.google.protobuf:protoc:4.30.2"
     }
     plugins {
         id("grpc") {
-            artifact = "io.grpc:protoc-gen-grpc-java:1.70.0"
+            artifact = "io.grpc:protoc-gen-grpc-java:1.71.0"
         }
         id("grpckt") {
             artifact = "io.grpc:protoc-gen-grpc-kotlin:1.4.1:jdk8@jar"
@@ -127,8 +127,8 @@ protobuf {
 val byteBuddyAgent = configurations.create("byteBuddyAgent")
 
 dependencies {
-    testImplementation("net.bytebuddy:byte-buddy-agent:1.17.2")
-    byteBuddyAgent("net.bytebuddy:byte-buddy-agent:1.17.2") { isTransitive = false }
+    testImplementation("net.bytebuddy:byte-buddy-agent:1.17.5")
+    byteBuddyAgent("net.bytebuddy:byte-buddy-agent:1.17.5") { isTransitive = false }
 }
 
 tasks {
